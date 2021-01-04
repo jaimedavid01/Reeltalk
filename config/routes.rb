8e55657657740166bd624f1adef62b3904e84bf2
jaimedavid01 <jaimedavid01@yahoo.com>
2021-01-04 17:13:52 -0500

    session routes and controller template

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :forums
   resources :movies
   resources :users
+
+  delete '/sessions/reset', to: 'sessions#reset_page_visits', as: 'page_reset'
+  delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
+  get '/sessions/new', to: 'sessions#new', as: 'new_session'
+  post '/sessions', to: 'sessions#create', as: 'sessions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
